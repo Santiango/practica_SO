2db966d29cb11b5f647b100c74f3e82a2b2d38ea
Santiago <santiagomarinocampo@gmail.com>
2021-12-19 18:30:06 -0500

    1

---
+++ Etapa1/psinfo.h
@@ -0,0 +1,7 @@
+#ifndef __PSINFO_H_
+#define __PSINFO_H_
+
+FILE* abrirArchivo(char proc[]);
+int imprimirInformacion(FILE *arch,FILE *out);
+
+#endif
---
+++ Etapa1/psinfoI.c
@@ -0,0 +1,64 @@
+#include <string.h>
+#include <stdio.h>
+#include "psinfo.h"
+#include <stdlib.h>
+
+
+
+FILE* abrirArchivo(char proc[])
+{
+
+    char archivo[1024];
+    sprintf(archivo, "/proc/%s/status", proc);
+    FILE *arch = fopen(archivo, "r");
+    return arch;
+}
+
+
+
+int imprimirInformacion(FILE *arch,FILE *out)
+{
+    char line[1024];
+    char *token, *auxiliar1;
+
+    while (fgets(line, 1024, arch))
+    {
+        token = strtok(line, ":");
+        auxiliar1 = strtok(NULL, "\n");
+        if (strcmp(token, "Name") == 0)
+        {
+            fprintf(out, "Nombre del proceso: %s\n", auxiliar1);
+        }
+        if (strcmp(token, "State") == 0)
+        {
+            fprintf(out, "Estado: %s\n", auxiliar1);
+        }
+        if (strcmp(token, "VmSize") == 0)
+        {
+            fprintf(out, "Tamaño total de la memoria: %s\n", auxiliar1);
+        }
+        if (strcmp(token, "VmExe") == 0)
+        {
+            fprintf(out, "\tTamaño total de la memoria de la región TEXT: %s\n", auxiliar1);
+        }
+        if (strcmp(token, "VmData") == 0)
+        {
+            fprintf(out, "\tTamaño de la memoria de la región DATA: %s\n", auxiliar1);
+        }
+        if (strcmp(token, "VmStk") == 0)
+        {
+            fprintf(out, "\tTamaño total de la memoria de la región STACK: %s\n", auxiliar1);
+        }
+        if (strcmp(token, "voluntary_ctxt_switches") == 0)
+        {
+            fprintf(out, "Número de cambios de contexto realizados (voluntarios - no voluntarios): %s", auxiliar1);
+        }
+        if (strcmp(token, "nonvoluntary_ctxt_switches") == 0)
+        {
+            fprintf(out, " - %s\n", auxiliar1);
+        }
+    }
+
+    return 0;
+}
+
